59baac20183726e8fc1ee5bdfdf4febb20d03a9b
mohlutfifadilah <mohlutfifadilah23@gmail.com>
2024-10-05 15:38:06 +0700

    done dashboard -1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Armada;
 use App\Models\Invoice;
+use App\Models\Pembayaran;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -13,16 +16,48 @@ class DashboardController extends Controller
         $count_invoice = Invoice::all()->count();
         $count_cash = Invoice::where('id_jenis_pembayaran', 1)->get()->count();
         $count_transfer = Invoice::where('id_jenis_pembayaran', 2)->get()->count();
-        // $count_tenagapengajar = Pegawai::all()->count();
-        // $count_kegiatan = Kegiatan::all()->count();
-        // $count_artikel = Artikel::all()->count();
+
+        $data1 = Invoice::select('id_jenis_pembayaran', DB::raw('count(*) as total'))
+                ->groupBy('id_jenis_pembayaran')
+                ->get();
+
+        // Ambil nama armada berdasarkan id_jenis_pembayaran
+        $labels1 = $data1->map(function($invoice) {
+            return Pembayaran::find($invoice->id_jenis_pembayaran)->nama_pembayaran;
+        });
+
+        $totals1 = $data1->pluck('total');
+
+        $armadas = Invoice::select('id_armada', DB::raw('SUM(tarif) as total_tarif'))
+                ->groupBy('id_armada')
+                ->get();
+
+        $income = Invoice::sum('tarif');
+
+        $count_armada = Armada::all()->count();
+
+        // $data2 = Invoice::select('id_armada', DB::raw('count(*) as total'))
+        //         ->groupBy('id_armada')
+        //         ->get();
+
+        // // Ambil nama armada berdasarkan id_armada
+        // $labels2 = $data2->map(function($invoice) {
+        //     return Armada::find($invoice->id_armada)->nama_armada;
+        // });
+
+        // $totals2 = $data2->pluck('total');
 
         return view('admin.dashboard', compact(
             'invoice',
             'count_invoice',
             'count_cash',
             'count_transfer',
-            // 'count_artikel'
+            'labels1',
+            'totals1',
+            'armadas',
+            'income',
+            'count_armada',
+            // 'totals2'
         ));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -47,9 +47,7 @@
                         @endphp
                             <li class="d-flex mb-4 pb-1">
                                 <div class="avatar flex-shrink-0 me-3">
-                                    <span class="avatar-initial rounded bg-label-primary"
-                                    ><i class="bx bx-mobile-alt"></i
-                                    ></span>
+                                     <img src="{{ asset('sneat/img/icons/unicons/cc-success.png') }}" alt="User" class="rounded" />
                                 </div>
                                 <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                     <div class="me-2">
@@ -67,12 +65,12 @@
                   </div>
                 </div>
                 <!--/ Order Statistics -->
-                <!-- Transactions -->
+                <!-- Pendapatan Armada -->
                 <div class="col-md-6 col-lg-4 order-2 mb-4">
                   <div class="card h-100">
                     <div class="card-header d-flex align-items-center justify-content-between">
-                      <h5 class="card-title m-0 me-2">Transactions</h5>
-                      <div class="dropdown">
+                      <h5 class="card-title m-0 me-2">Pendapatan Armada</h5>
+                      {{-- <div class="dropdown">
                         <button
                           class="btn p-0"
                           type="button"
@@ -88,100 +86,30 @@
                           <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
                           <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
                         </div>
-                      </div>
+                      </div> --}}
                     </div>
                     <div class="card-body">
                       <ul class="p-0 m-0">
-                        <li class="d-flex mb-4 pb-1">
-                          <div class="avatar flex-shrink-0 me-3">
-                            <img src="{{ asset('sneat/img/icons/unicons/paypal.png') }}" alt="User" class="rounded" />
-                          </div>
-                          <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                            <div class="me-2">
-                              <small class="text-muted d-block mb-1">Paypal</small>
-                              <h6 class="mb-0">Send money</h6>
-                            </div>
-                            <div class="user-progress d-flex align-items-center gap-1">
-                              <h6 class="mb-0">+82.6</h6>
-                              <span class="text-muted">USD</span>
-                            </div>
-                          </div>
-                        </li>
-                        <li class="d-flex mb-4 pb-1">
-                          <div class="avatar flex-shrink-0 me-3">
-                            <img src="{{ asset('sneat/img/icons/unicons/wallet.png') }}" alt="User" class="rounded" />
-                          </div>
-                          <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                            <div class="me-2">
-                              <small class="text-muted d-block mb-1">Wallet</small>
-                              <h6 class="mb-0">Mac'D</h6>
-                            </div>
-                            <div class="user-progress d-flex align-items-center gap-1">
-                              <h6 class="mb-0">+270.69</h6>
-                              <span class="text-muted">USD</span>
-                            </div>
-                          </div>
-                        </li>
-                        <li class="d-flex mb-4 pb-1">
-                          <div class="avatar flex-shrink-0 me-3">
-                            <img src="{{ asset('sneat/img/icons/unicons/chart.png') }}" alt="User" class="rounded" />
-                          </div>
-                          <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                            <div class="me-2">
-                              <small class="text-muted d-block mb-1">Transfer</small>
-                              <h6 class="mb-0">Refund</h6>
-                            </div>
-                            <div class="user-progress d-flex align-items-center gap-1">
-                              <h6 class="mb-0">+637.91</h6>
-                              <span class="text-muted">USD</span>
-                            </div>
-                          </div>
-                        </li>
-                        <li class="d-flex mb-4 pb-1">
-                          <div class="avatar flex-shrink-0 me-3">
-                            <img src="{{ asset('sneat/img/icons/unicons/cc-success.png') }}" alt="User" class="rounded" />
-                          </div>
-                          <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                            <div class="me-2">
-                              <small class="text-muted d-block mb-1">Credit Card</small>
-                              <h6 class="mb-0">Ordered Food</h6>
-                            </div>
-                            <div class="user-progress d-flex align-items-center gap-1">
-                              <h6 class="mb-0">-838.71</h6>
-                              <span class="text-muted">USD</span>
-                            </div>
-                          </div>
-                        </li>
-                        <li class="d-flex mb-4 pb-1">
-                          <div class="avatar flex-shrink-0 me-3">
-                            <img src="{{ asset('sneat/img/icons/unicons/wallet.png') }}" alt="User" class="rounded" />
-                          </div>
-                          <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                            <div class="me-2">
-                              <small class="text-muted d-block mb-1">Wallet</small>
-                              <h6 class="mb-0">Starbucks</h6>
-                            </div>
-                            <div class="user-progress d-flex align-items-center gap-1">
-                              <h6 class="mb-0">+203.33</h6>
-                              <span class="text-muted">USD</span>
-                            </div>
-                          </div>
-                        </li>
-                        <li class="d-flex">
-                          <div class="avatar flex-shrink-0 me-3">
-                            <img src="{{ asset('sneat/img/icons/unicons/cc-warning.png') }}" alt="User" class="rounded" />
-                          </div>
-                          <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                            <div class="me-2">
-                              <small class="text-muted d-block mb-1">Mastercard</small>
-                              <h6 class="mb-0">Ordered Food</h6>
-                            </div>
-                            <div class="user-progress d-flex align-items-center gap-1">
-                              <h6 class="mb-0">-92.45</h6>
-                              <span class="text-muted">USD</span>
-                            </div>
-                          </div>
-                        </li>
+                        @foreach ($armadas as $a)
+                            @php
+                                $armada = \App\Models\Armada::find($a->id_armada);
+                            @endphp
+                            <li class="d-flex mb-4 pb-1">
+                                <div class="avatar flex-shrink-0 me-3">
+                                    <img src="{{ asset('sneat/img/icons/unicons/car-solid-24.png') }}" alt="Credit Card" class="rounded" />
+                                </div>
+                                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                    <div class="me-2">
+                                    <small class="text-muted d-block mb-1">Armada</small>
+                                    <h6 class="mb-0">{{ $armada->nama_armada }}</h6>
+                                    </div>
+                                    <div class="user-progress d-flex align-items-center gap-1">
+                                    <h6 class="mb-0">@currency($a->total_tarif)</h6>
+                                    <span class="text-muted">++</span>
+                                    </div>
+                                </div>
+                            </li>
+                        @endforeach
                       </ul>
                     </div>
                   </div>
@@ -189,75 +117,13 @@
                 <!--/ Transactions -->
                 <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
                   <div class="row">
-                    <div class="col-6 mb-4">
-                      <div class="card">
-                        <div class="card-body">
-                          <div class="card-title d-flex align-items-start justify-content-between">
-                            <div class="avatar flex-shrink-0">
-                              <img src="{{ asset('sneat/img/icons/unicons/money-regular-24.png') }}" alt="Credit Card" class="rounded" />
-                            </div>
-                            <div class="dropdown">
-                              <button
-                                class="btn p-0"
-                                type="button"
-                                id="cardOpt4"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                              >
-                                <i class="bx bx-dots-vertical-rounded"></i>
-                              </button>
-                              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
-                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                              </div>
-                            </div>
-                          </div>
-                          <span class="d-block mb-1">Payments</span>
-                          <h3 class="card-title text-nowrap mb-2">$2,456</h3>
-                          <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-6 mb-4">
-                      <div class="card">
-                        <div class="card-body">
-                          <div class="card-title d-flex align-items-start justify-content-between">
-                            <div class="avatar flex-shrink-0">
-                              <img src="{{ asset('sneat/img/icons/unicons/car-solid-24.png') }}" alt="Credit Card" class="rounded" />
-                            </div>
-                            <div class="dropdown">
-                              <button
-                                class="btn p-0"
-                                type="button"
-                                id="cardOpt1"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                              >
-                                <i class="bx bx-dots-vertical-rounded"></i>
-                              </button>
-                              <div class="dropdown-menu" aria-labelledby="cardOpt1">
-                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                              </div>
-                            </div>
-                          </div>
-                          <span class="fw-semibold d-block mb-1">Transactions</span>
-                          <h3 class="card-title mb-2">$14,857</h3>
-                          <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- </div>
-    <div class="row"> -->
                     <div class="col-12 mb-4">
                       <div class="card">
                         <div class="card-body">
                           <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
                             <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
                               <div class="card-title">
-                                <h5 class="text-nowrap mb-2">Profile Report</h5>
+                                <h5 class="text-nowrap mb-2">Pemasukan</h5>
                                 <span class="badge bg-label-warning rounded-pill">Year 2021</span>
                               </div>
                               <div class="mt-sm-auto">
@@ -272,10 +138,90 @@
                         </div>
                       </div>
                     </div>
+                    <div class="col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img src="{{ asset('sneat/img/icons/unicons/money-regular-24.png') }}" alt="Credit Card" class="rounded" />
+                            </div>
+                            {{-- <div class="dropdown">
+                              <button
+                                class="btn p-0"
+                                type="button"
+                                id="cardOpt4"
+                                data-bs-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                              >
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                              </button>
+                              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
+                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                              </div>
+                            </div> --}}
+                          </div>
+                          <span class="d-block mb-1">Total Pemasukan</span>
+                          <h3 class="card-title text-nowrap mb-2">@currency($income)</h3>
+                          {{-- <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small> --}}
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img src="{{ asset('sneat/img/icons/unicons/car-solid-24.png') }}" alt="Credit Card" class="rounded" />
+                            </div>
+                            {{-- <div class="dropdown">
+                              <button
+                                class="btn p-0"
+                                type="button"
+                                id="cardOpt1"
+                                data-bs-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                              >
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                              </button>
+                              <div class="dropdown-menu" aria-labelledby="cardOpt1">
+                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                              </div>
+                            </div> --}}
+                          </div>
+                          <span class="fw-semibold d-block mb-1">Total Armada</span>
+                          <h3 class="card-title mb-2">{{ $count_armada }}</h3>
+                          {{-- <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small> --}}
+                        </div>
+                      </div>
+                    </div>
+                    <!-- </div>
+    <div class="row"> -->
+
                   </div>
                 </div>
               </div>
               </div>
             </div>
             <!-- / Content -->
+
+            <script>
+                var labels1 = @json($labels1);  // Mengirimkan data dari controller ke JavaScript
+                var labelsLength1 = labels1.length;  // Mendapatkan panjang data
+
+                var totals1 = @json($totals1);
+
+                function getRandomColor() {
+                    var letters = '0123456789ABCDEF';
+                    var color = '#';
+                    for (var i = 0; i < 6; i++) {
+                        color += letters[Math.floor(Math.random() * 16)];
+                    }
+                    return color;
+                }
+            </script>
+
 @endsection
